a53eb9973662ffc3dc730a8239803340c75a7da1
ccccccky <1003448647@qq.com>
2019-05-08 20:35:52 +0800

    anjing is more handsome than lige, your skill in playing dota is awesome!

---
+++ homework.py
@@ -0,0 +1,12 @@
+n = 0
+
+
+with open('C:/Users/陈可悦/Desktop/文件/Python/in anjing class/week 10/text-file-process-ccccccky/text-file-process/log_files/201811113019.log', encoding = 'utf8') as f:
+    for line in f :
+        line = line.split('：' , 3)
+        # print(line[2])
+        if (line[2] == '201811113019, 时间'):
+            n += 1
+            # print(n)
+print(n)
+
