802907cb4725cc3cf747b576473a69a49e815157
ccccccky <1003448647@qq.com>
2019-05-10 15:29:50 +0800

    homework.py

--- homework1.py
+++ homework1.py
@@ -1,12 +1,10 @@
+
 n = 0
-
-
 with open('C:/Users/陈可悦/Desktop/文件/Python/in anjing class/week 10/text-file-process-ccccccky/text-file-process/log_files/201811113019.log', encoding = 'utf8') as f:
     for line in f :
         line = line.split('：' , 3)
         # print(line[2])
-        if (line[2] == '201811113019, 时间'):
+        if( line[2] == '201811113019, 时间' ):
             n += 1
-            # print(n)
 print(n)
 
